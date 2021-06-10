103a8993e9641e855b131c49bc795e7d559ebdb8
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-09 23:16:39 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(number1, number2){
+    return number1+number2;
+}
+
+function subtract(number1, number2){
+    return number1-number2;
+}
+
+function multiply(number1, number2){
+    return number1*number2;
+}
+
+function divide(number1, number2){
+    return number1/number2;
+}
+
+function increment(number){
+    return number+1;
+}
+
+function decrement(number){
+    return number-1;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
